d410b26162e27cee32bde54559a3e5bb71466a45
nngocanh1401-cloud <nngocanh.1401@gmail.com>
2026-08-28 14:29:38 +0700

    len dau trang

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // js/main.js
 import { initNav, initHeaderOnScroll, initToTop } from "./nav.js";
-import { initTheme } from "./theme.js";
-import { initFaq } from "./faq.js";
-import { initPricing } from "./pricing.js";
-import { initSlider } from "./slider.js";
-import { initReveal } from "./reveal.js";
+// import { initTheme } from "./theme.js";
+// import { initFaq } from "./faq.js";
+// import { initPricing } from "./pricing.js";
+// import { initSlider } from "./slider.js";
+// import { initReveal } from "./reveal.js";
 
 initNav();
-initTheme();
-initFaq();
-initPricing();
-initSlider();
-initReveal();
+initHeaderOnScroll();
+initToTop();
+// initTheme();
+// initFaq();
+// initPricing();
+// initSlider();
+// initReveal();
